2e4af0702a113f9c6fdf2282e2340e21d188b454
Dominid Muñoz Eslava <dominidzero@gmail.com>
2025-08-04 17:23:53 -0500

    [DME] - 04082025 USER AND ROL TABLE BACKEND

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.public_home, name='public_home'),  # /public/
+    path('about/', views.public_about, name='public_about'),  # /public/about/
+    # ...otros endpoints públicos
 ]
--- webapp/urls.py
+++ webapp/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # Para el home
-    path('sesiones/', include('modules.public.sesiones.routes.urls')),  # Para sesiones
+    path('', include('core.urls')),  # Home
+    path('sesiones/', include('modules.public.sesiones.routes.urls')),
+    path('public/', include('modules.public.urls')),  # Todo lo de /public/
+    path('adminroutes/', include('modules.adminroutes.urls')),  # Todo lo de /adminroutes/
 ]
