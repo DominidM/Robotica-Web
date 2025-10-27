3ff91a88224176b66b4fb1ad8539aa9abdeeda15
renzukizzz <arandarenzo659@gmail.com>
2025-10-27 03:54:34 -0500

    [RAG] - 27102025 UPDATE AZURE

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ASKINET\Desktop\REPOSITORIES\Robotica-Web\.venv
+set VIRTUAL_ENV=C:\Users\renzi\OneDrive\Documentos\GitHub\Robotica-Web\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
